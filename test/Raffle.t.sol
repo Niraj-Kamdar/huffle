801cb5d7d93feefefbf6982a82fa7e5301d51860
Niraj Kamdar <nirajkamdar27@pm.me>
2026-01-02 20:38:16 +0400

    feat: record players

--- test/Raffle.t.sol
+++ test/Raffle.t.sol
@@ -7,26 +7,43 @@ import "forge-std/console.sol";
 
 interface Raffle {
     function getEntranceFee() external view returns (uint256);
+    function enterRaffle() external payable;
+    function getPlayer(uint256) external view returns (address);
 }
 
-
 contract RaffleTest is Test {
+    /// @dev Address of the SimpleStore contract.
+    event RaffleEnter(address indexed player);
+
     Raffle public raffle;
     uint256 public constant raffleEntranceFee = 1 ether;
+    
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
 
     /// @dev Setup the testing environment.
     function setUp() public {
-        console.logBytes(bytes.concat(abi.encode(raffleEntranceFee)));
         raffle = Raffle(
             HuffDeployer
                 .config()
                 .with_args(bytes.concat(abi.encode(raffleEntranceFee)))
                 .deploy("Raffle")
         );
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
     }
 
     function testGetEntranceFee() public {
         uint256 actualEntranceFee = raffle.getEntranceFee();
         assertEq(actualEntranceFee, raffleEntranceFee, "Entrance fee is not same as the one we set");
+    }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        // Arrange
+        vm.prank(PLAYER);
+        // Act
+        raffle.enterRaffle{value: raffleEntranceFee}();
+        // Assert
+        address playerRecorded = raffle.getPlayer(0);
+        assert(playerRecorded == PLAYER);
     }
 }
